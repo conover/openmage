e6bf1e253a5bf35fd504f595e5fecc7357bc4991
Chris Conover <clcono@gmail.com>
2011-03-08 16:09:06 -0500

    Added server communication

--- client/_assets/js/entity.js
+++ client/_assets/js/entity.js
@@ -1,15 +1,17 @@
 var EntityManager = function() {
-    var that = this,
-        entities = [],
-        entity_count = 0;
+    var that            = this,
+        entity_count    = 0;;
+    
+    this.entities        = [],
+        
         
     this.add_entity = function(entity) {
        if(entity.types.indexOf('entity') == -1) {
            throw 'Specified object must be an entity type'
        }
        entity_count += 1;
-       entity.id = entity_count;
-       entities.push(entity)
+       entity.id = Math.floor(Math.random() * 9999999);
+       that.entities.push(entity)
     }
     
     this.remove_entity = function(entity) {
@@ -17,7 +19,7 @@ var EntityManager = function() {
             throw 'Specified object must be an entity type'
         }
         var remove_index = null;
-        entities.forEach(function(val, index, arr) {
+        that.entities.forEach(function(val, index, arr) {
             if(val.id == entity.id) {
                 remove_index = val.id;
             }
@@ -25,33 +27,61 @@ var EntityManager = function() {
         if(remove_index == null) {
             throw 'Specified entity could not be found to be deleted';
         }
-        entities.splice(remove_index, 1);
+        that.entities.splice(remove_index, 1);
     }
     
-    this.notify_mouse_drag = function(x, y) {
-        entities.forEach(function(val, index, arr) {
-            if(typeof val.on_mouse_drag == 'function') {
-                val.on_mouse_drag(x, y);
-            }
-        });
-    }
-    this.maintain = function() {
-        entities.forEach(function(val, index, arr) {
+    this.maintain = function(context, ws_queue) {
+        
+        that.entities.forEach(function(entity) {
             //CONTEXT.save();
-            val.draw();
+            entity.draw(context);
+            
+            var e_json = entity.to_json()
+            if(entity.prev_status != e_json) {
+                ws_queue.push(e_json);
+                entity.prev_status = e_json;
+            }
             //CONTEXT.restore();
         })
     }
 }
 
 var Entity = function() { // Base from which all entities inherit
-    var that = this;
+    var that        = this;
     
-    this.types.push('entity')
-
-    this.id     = null; // Assigned by the entity manager
-    this.loc    = new Point(0, 0);
-    this.dim    = new Dimension(0,0);
+    this.id         = null; // Assigned by the entity manager
+    this.loc        = new Point(0, 0);
+    this.dim        = new Dimension(0,0);
+    
+    this.sync_vars  = ['dim'];
+    
+    this.types = ['entity']
+    
+    this.prev_status = ''
+    
+    this.to_json = function() {
+        return that.id + '|' + that.types[that.types.length - 1]+ '|' + that.toJSONString(that.sync_vars);
+    }
+    
+    this.from_json = function(json_string) {
+        var sync_obj = json_string.parseJSON()
+        for(var prop in sync_obj) {
+            if(prop == 'loc') {
+                loc = sync_obj[prop]
+                this[prop] = new Point(loc[0], loc[1])
+            } else if(prop == 'dim') {
+                dim = sync_obj[prop]
+                this[prop] = new Dimension(dim[0], dim[1])
+            } else if(prop == 'target_loc') {
+                loc = sync_obj[prop]
+                if(loc != null) {
+                    this[prop] = new Point(loc[0], loc[1])
+                }
+            } else {
+                this[prop] = sync_obj[prop]
+            }
+        }
+    }
     
     this.set_dim = function(dim) {
         if(dim.types.indexOf('dimension') == -1) {
@@ -59,14 +89,14 @@ var Entity = function() { // Base from which all entities inherit
         }
         that.dim = dim;
     }
-    
+
     this.set_loc = function(loc) {
         if(loc.types.indexOf('point') == -1) {
             throw 'Specifid object must be a point type'
         }
         that.loc = loc;
     }
-    
+
     this.get_bounding_box = function () {
         return  [   that.loc.x - this.dim.width, this.loc.y - this.dim.height,
                     that.loc.x + this.dim.width, this.loc.y + this.dim.height,
@@ -74,20 +104,25 @@ var Entity = function() { // Base from which all entities inherit
                     that.loc.x + this.dim.width, this.loc.y - this.dim.height
                 ]
     }
-    
+
     this.draw = function() {
         throw 'Draw should be implemented for each type of entity'
-    }
+    }    
 }
 
+
 var Mage = function() {
-    var that            = this,
-        
-        target_loc      = null, // Point the user wants to move to
-        mov_speed       = 2.7,   // Default movement speed
-        health          = 75,   // Starting health
-        shield          = 0;    // Starting shield
+    Entity.call(this);
     
+    var that            = this;
+    
+    this.target_loc      = null; // Point the user wants to move to
+    this.mov_speed       = 2.7,  // Default movement speed
+    this.health          = 75,   // Starting health
+    this.shield          = 0;    // Starting shield    
+    
+    this.sync_vars.push('target_loc', 'mov_speed', 'health', 'shield', 'color')
+        
     this.set_dim(new Dimension(15, 35)) // Default mage size
     
     this.types.push('mage')
@@ -102,22 +137,22 @@ var Mage = function() {
         that.color = color;
     }
     
-    this.draw = function() {
+    this.draw = function(context) {
         
         var draw_loc = new Point(0, 0); // New location to draw the mage
         
-        if(target_loc != null) {
+        if(that.target_loc != null) {
             
-            var abs_diff_x = Math.abs(that.loc.x - target_loc.x),
-                abs_diff_y = Math.abs(that.loc.y - target_loc.y);
+            var abs_diff_x = Math.abs(that.loc.x - that.target_loc.x),
+                abs_diff_y = Math.abs(that.loc.y - that.target_loc.y);
             
             if(abs_diff_x < 10 && abs_diff_y < 10) { // How close does the target have to be to stop moving
                 draw_loc = that.loc;
-                target_loc = null;
+                that.target_loc = null;
             } else {
                 
                 // Which directions to mov in
-                var bearing = -1 * Math.atan2(target_loc.y - that.loc.y, target_loc.x - that.loc.x);
+                var bearing = -1 * Math.atan2(that.target_loc.y - that.loc.y, that.target_loc.x - that.loc.x);
                 
                 // Without a smoothing factor, the animation with look really jerky
                 var     x_mov_speed     = null,
@@ -131,12 +166,12 @@ var Mage = function() {
                     }
                 }
                     
-                x_mov_speed = y_mov_speed = mov_speed * smooth_factor;
+                x_mov_speed = y_mov_speed = that.mov_speed * smooth_factor;
             
                 if(abs_diff_x > abs_diff_y) {
-                  x_mov_speed = mov_speed * 1.5
+                  x_mov_speed = that.mov_speed * 1.5
                 } else {
-                  y_mov_speed = mov_speed * 1.5
+                  y_mov_speed = that.mov_speed * 1.5
                 }
                 
                 if(bearing == 0) { // Move right
@@ -178,41 +213,41 @@ var Mage = function() {
             draw_loc = that.loc;
         }
         
-        CONTEXT.fillStyle = that.color
-        CONTEXT.fillRect(draw_loc.x, draw_loc.y,that.dim.width,that.dim.height);
-        draw_health_bar();
-        draw_shield_bar();
-        draw_elements();
+        context.fillStyle = that.color
+        context.fillRect(draw_loc.x, draw_loc.y,that.dim.width,that.dim.height);
+        draw_health_bar(context);
+        draw_shield_bar(context);
+        draw_elements(context);
     } 
     
     
-    function draw_health_bar() {
+    function draw_health_bar(context) {
         // Health bar conists of a red bar on top of a black bar
-        CONTEXT.fillStyle = 'rgb(0,0,0)'
+        context.fillStyle = 'rgb(0,0,0)'
         vert_location = that.loc.x - Math.round(that.dim.width / 2) - 10
-        CONTEXT.fillRect(vert_location,that.loc.y + 39,50, 5)
+        context.fillRect(vert_location,that.loc.y + 39,50, 5)
         
-        CONTEXT.fillStyle = 'rgb(255,0,0)'
+        context.fillStyle = 'rgb(255,0,0)'
         vert_location = that.loc.x - Math.round(that.dim.width / 2) - 10
-        CONTEXT.fillRect(vert_location,that.loc.y + 39,Math.round(health / 2), 5)
+        context.fillRect(vert_location,that.loc.y + 39,Math.round(that.health / 2), 5)
     }
     
-    function draw_shield_bar() {
-        CONTEXT.fillStyle = 'rgb(100,100,100)'
+    function draw_shield_bar(context) {
+        context.fillStyle = 'rgb(100,100,100)'
         vert_location = that.loc.x - Math.round(that.dim.width / 2) - 10
-        CONTEXT.fillRect(vert_location,that.loc.y + 46,50, 4)
+        context.fillRect(vert_location,that.loc.y + 46,50, 4)
         
         
-        CONTEXT.fillStyle = 'rgb(255,255,255)'
+        context.fillStyle = 'rgb(255,255,255)'
         vert_location = that.loc.x - Math.round(that.dim.width / 2) - 10
-        CONTEXT.fillRect(vert_location,that.loc.y + 46,Math.round(shield / 2), 4)
+        context.fillRect(vert_location,that.loc.y + 46,Math.round(that.shield / 2), 4)
         
-        CONTEXT.strokeStyle = 'rgba(100,100,100, .4)';
+        context.strokeStyle = 'rgba(100,100,100, .4)';
         vert_location = that.loc.x - Math.round(that.dim.width / 2) - 10
-        CONTEXT.strokeRect(vert_location,that.loc.y + 46,50, 4)
+        context.strokeRect(vert_location,that.loc.y + 46,50, 4)
     }
     
-    function draw_elements() {
+    function draw_elements(context) {
         // water
         // fire
         // cold
@@ -226,17 +261,15 @@ var Mage = function() {
         
             vert_location = that.loc.x - Math.round(that.dim.width / 2) - 5
             
-            CONTEXT.strokeStyle = 'rgba(100,100,100,.5)';
-            CONTEXT.fillStyle = 'rgba(175,175,175,.3)';
-            CONTEXT.beginPath();
-            CONTEXT.arc(vert_location + (i * 10.3), that.loc.y + 57 , 4, 0, Math.PI * 2, true);
-            CONTEXT.closePath();
-            CONTEXT.fill();
-            CONTEXT.stroke();
+            context.strokeStyle = 'rgba(100,100,100,.5)';
+            context.fillStyle = 'rgba(175,175,175,.3)';
+            context.beginPath();
+            context.arc(vert_location + (i * 10.3), that.loc.y + 57 , 4, 0, Math.PI * 2, true);
+            context.closePath();
+            context.fill();
+            context.stroke();
         }
     
     }
-    this.on_mouse_drag = function(x, y) {target_loc = new Point(x,y);}
+    this.move = function(x, y) {that.target_loc = new Point(x,y);}
 }
-
-Mage.inherits(Entity);
--- client/_assets/js/util.js
+++ client/_assets/js/util.js
@@ -1,42 +1,18 @@
-Object.prototype.types = [];
+if(typeof(console.log) == 'undefined') {
+    console = function() {
+        this.log = function(s) {alert(s);}
+    }
+}
 
-
-Function.prototype.method = function (name, func) {
-    this.prototype[name] = func;
-    return this;
-};
-Function.method('inherits', function (parent) {
-    var d = {}, p = (this.prototype = new parent());
-    this.method('uber', function uber(name) {
-        if (!(name in d)) {
-            d[name] = 0;
-        }        
-        var f, r, t = d[name], v = parent.prototype;
-        if (t) {
-            while (t) {
-                v = v.constructor.prototype;
-                t -= 1;
-            }
-            f = v[name];
-        } else {
-            f = p[name];
-            if (f == this[name]) {
-                f = v[name];
-            }
-        }
-        d[name] += 1;
-        r = f.apply(this, Array.prototype.slice.apply(arguments, [1]));
-        d[name] -= 1;
-        return r;
-    });
-    return this;
-});
+Object.prototype.types = []
 
 var Point = function(x, y) {
     var that = this;
     this.types.push('point')
     this.x = x;
     this.y = y;
+    
+    this.toJSON = function() { return [this.x,this.y];}
 }
 
 var Dimension = function(width, height) {
@@ -44,4 +20,6 @@ var Dimension = function(width, height) {
     this.types.push('dimension');
     this.width = width;
     this.height = height;
+    
+    this.toJSON = function() {return [this.width, this.height]}
 }
